1202d5fde575a30fe80a42ea2381ea6ae364df9c
Renming Zhang <kevin110299@126.com>
2023-02-08 10:18:20 -0500

    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/flashcard/FlashCard.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.flashcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_card)
+    }
+}
