fbc686e6ead9e0f9331f86f943b39a69a682ffb0
Renming Zhang <kevin110299@126.com>
2023-02-08 16:40:38 -0500

    Login logic finished, layout bug fixed

--- app/src/main/java/com/bignerdranch/android/flashcard/FlashCard.kt
+++ app/src/main/java/com/bignerdranch/android/flashcard/FlashCard.kt
@@ -2,10 +2,14 @@ package com.bignerdranch.android.flashcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.flashcard.databinding.ActivityFlashCardBinding
 
 class FlashCard : AppCompatActivity() {
+    private lateinit var binding:ActivityFlashCardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_card)
+        binding=ActivityFlashCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/bignerdranch/android/flashcard/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/flashcard/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.bignerdranch.android.flashcard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import com.bignerdranch.android.flashcard.databinding.ActivityFlashCardBinding
+import com.bignerdranch.android.flashcard.databinding.ActivityLoginBinding
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private var username=""
+    private var password=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+        setupListeners()
+
     }
+
+    fun validateId(): Boolean {
+        username=binding.userNameEditText.text.toString()
+        password=binding.passwordEditText.text.toString()
+        if(username.equals("Admin")&& password.equals("123456"))
+            return true
+        return false
+    }
+    fun setupListeners(){
+        binding.submitButton.setOnClickListener {
+            if(validateId())
+            {
+                var it=Intent(this,FlashCard::class.java)
+                startActivity(it);
+                //start intent
+            }else
+            {
+                Toast.makeText(this, R.string.login_error, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
